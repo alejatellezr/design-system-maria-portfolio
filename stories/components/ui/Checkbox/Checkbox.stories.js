0a94e7c571f9fd1802c55cef3eae4d1a77b5db90
matellezr <matellezr@dane.gov.co>
2025-07-06 11:45:29 -0500

    updating checbox control behaviour

--- stories/components/ui/Checkbox/Checkbox.stories.js
+++ stories/components/ui/Checkbox/Checkbox.stories.js
@@ -1,31 +1,45 @@
-// Checkbox.stories.js
-import React, { useState } from 'react';
-import Checkbox from './Checkbox';
-import './_checkbox.scss';
+import React, { useState, useEffect } from 'react';
+import Checkbox from "./Checkbox";
+import "./_checkbox.scss";
 
 export default {
-  title: 'UI/Checkbox',
+  title: "UI/Checkbox",
   component: Checkbox,
   argTypes: {
-    checked: { control: 'boolean' },
-    disabled: { control: 'boolean' },
-    label: { control: 'text' },
+    checked: { control: "boolean" },
+    indeterminate: { control: "boolean" },
+    disabled: { control: "boolean" },
+    label: { control: "text" },
     labelPosition: {
-      control: { type: 'radio' },
-      options: ['left', 'right'],
-      defaultValue: 'right',
+      control: { type: "radio" },
+      options: ["left", "right"],
+      defaultValue: "right",
     },
   },
 };
 
 const Template = (args) => {
   const [isChecked, setIsChecked] = useState(args.checked);
+  const [isIndeterminate, setIsIndeterminate] = useState(args.indeterminate);
 
+  useEffect(() => {
+    setIsChecked(args.checked);
+  }, [args.checked]);
+
+  useEffect(() => {
+    setIsIndeterminate(args.indeterminate);
+  }, [args.indeterminate]);
+
+  const handleChange = (checked) => {
+    setIsChecked(checked);
+    setIsIndeterminate(false);
+  };
   return (
     <Checkbox
       {...args}
       checked={isChecked}
-      onChange={setIsChecked}
+      indeterminate={isIndeterminate}
+      onChange={handleChange}
     />
   );
 };
@@ -33,14 +47,26 @@ const Template = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   checked: false,
-  label: 'Accept terms',
-  labelPosition: 'right',
+  indeterminate: false,
+  label: "Accept terms",
+  labelPosition: "right",
   disabled: false,
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   checked: true,
-  label: 'Disabled checkbox',
+  indeterminate: false,
+  label: "Disabled checkbox",
+  labelPosition: "right",
   disabled: true,
+};
+
+export const Indeterminate = Template.bind({});
+Indeterminate.args = {
+  checked: false,
+  indeterminate: true,
+  label: "Partially selected",
+  labelPosition: "right",
+  disabled: false,
 };
